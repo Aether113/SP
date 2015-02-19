3c63eba6b43317e5091ce523d1a1a60553bb04f7
Aether113 <vangindertael@gmail.com>
2015-02-19 14:02:55 +0100

    Labo 1 SP compleet

---
+++ Exercise 1/main.c
@@ -0,0 +1,12 @@
+//excercise 1
+//Author: Jens Vangindertael
+
+#include <stdlib.h>
+
+int main()
+{
+  system("clear");
+  system("ls -l > files.txt");
+  system("sort files.txt | more");
+  return 0;
+}
