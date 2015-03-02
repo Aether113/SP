4dabe388f612b6fe93c97ab3ac9ef4ff36770709
Aether113 <vangindertael@gmail.com>
2015-03-02 23:47:59 +0100

    lab 3 ma_malloc spaghetti

--- Lab3/ma_malloc.c
+++ Lab3/ma_malloc.c
@@ -1,10 +1,12 @@
 #include "ma_malloc.h"
 #include <stdlib.h>
+#include <string.h>
 
 #define INITIAL_MEM_SIZE 500
 
 static char memory_block[INITIAL_MEM_SIZE];
-int i;
+int i,j,k;
+int *block_ptr;
 
 struct header{
   char flag;
@@ -19,10 +21,28 @@ void ma_init(){
 }
 
 void* ma_malloc(size tsize){
-  short free = 1;
-  while(free)
-  {
-    int *ptr = &memory_block[0];
+  //Check for header file (header struct), skip data chunk size and check again if full
+  //header = char + int = 5 bytes
+  i = 0;
+  while(i < 500){
+    if(memory_block[i] == 'F'){
+      for(j = i+1; j < i+5; j++){         // k = size of data chunk
+        k = 10 * k + memory_block[i];
+      }
+      i = i + k; //jump ahead chunck size (tsize) to next header.
+    }
+    else{
+      //Moet wrs niet tsize zijn maar waarde van k
+      memory_block[i] = 'F';
+      memory_block[i+1] = tsize%10/1;
+      tsize = tsize - tsize%10;
+      memory_block[i+2] = tsize%100/10;
+      tsize = tsize - tsize%100;
+      memory_block[i+3] = tsize%1000/100;
+      tsize = tsize - tsize%1000;
+      memory_block[i+4] = tsize%10000/1000;
+      tsize = tsize - tsize%10000;
+    }
   }
 }
 
